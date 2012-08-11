aec3a32b971cb1a95ad8968072ef8a9d5cb55587
mattcolf.com <mattcolf.com@n22.c09.mtsvc.net>
2012-08-10 22:20:44 -0700

    Initial commit.

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title>CashTrack</title>
+<style type="text/css">
+	a:link { color: white; text-decoration: none; }
+	a:visited { color: white; text-decoration: none; }
+</style>
+</head>
+<body bgcolor="black">
+<center>
+<img src="home.jpg" width="800px" alt="bill share info graphic" />
+<br />
+<p style="color:white; font-family:Sans-serif; font-size: 
+12px;">
+<br />
+<a href="eos_notice.html">End of Service Notice</a>
+<br /><br />
+© 2009
+</p>
+</center>
+
+<!-- Google Analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-9117449-9");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
